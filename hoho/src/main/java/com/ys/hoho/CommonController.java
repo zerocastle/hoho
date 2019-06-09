0eb5199255cbe63a650ed39325686b48a751f517
kys <romg321@naver.com>
2019-06-09 17:43:04 +0900

    0609 커밋
    0609 커밋

---
+++ hoho/src/main/java/com/ys/hoho/CommonController.java
@@ -0,0 +1,37 @@
+package com.ys.hoho;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommonController {
+
+	private static final Logger log = LoggerFactory.getLogger(CommonController.class);
+
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth, Model model) {
+
+		log.info("access denied : " + auth);
+
+		model.addAttribute("msg", "Access Denied");
+
+	}
+
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		log.info("error : " + error);
+		log.info("logout : " + logout);
+
+		if (error != null) {
+			model.addAttribute("error", "Login Error check your Account");
+		}
+		if (logout != null) {
+			model.addAttribute("logout", "Logout !!");
+		}
+	}
+
+}
